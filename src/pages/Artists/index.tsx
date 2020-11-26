ad7f0bdcd929e27eada0d47a7b28f56e785956c2
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-11-26 13:43:32 +0100

    fetch data artist search

---
+++ src/pages/Artists/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { apiUrl } from "../../config/constants";
+import { Card, CardDeck, CardGroup } from "react-bootstrap";
+
+export default function Artists() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  function submit(event: React.MouseEvent<HTMLInputElement>) {
+    async function fetchData() {
+      const response = await axios.get(`${apiUrl}/artists`, {
+        params: { searchTerm: searchTerm },
+      });
+      //   console.log("response", response.data._embedded.results);
+      const results = response?.data._embedded.results;
+      //   console.log(results);
+      //@ts-ignore
+      const filteredResults = results.filter((res) => res.type === "artist");
+      console.log(filteredResults);
+      setResults(filteredResults);
+    }
+    fetchData();
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <Container>
+        <Form>
+          <Form.Group controlId="formBasicSearch">
+            <Form.Label>Search for artist:</Form.Label>
+            <Form.Control
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+              type="searchTerm"
+              required
+            />
+            <Button variant="primary" type="submit" onClick={submit}>
+              Search
+            </Button>
+          </Form.Group>
+        </Form>
+      </Container>
+      <CardDeck style={{ display: "flex", flexDirection: "row" }}>
+        {results.map((res) => {
+          return (
+            <Card style={{ width: "200px" }}>
+              {/* 
+  // @ts-ignore */}
+              {/* <Card.Img variant="top" src={res._links.thumbnail.href} /> */}
+              <Card.Body>
+                {/* 
+  // @ts-ignore */}
+                <Card.Title>{res.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </CardDeck>
+    </div>
+  );
+}
