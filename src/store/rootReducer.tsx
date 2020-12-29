4ba001b8e2c8aa0aaf9bb0d514bd6e2cfdd28c83
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-12-29 14:11:13 +0100

    add appState slice

--- src/store/rootReducer.tsx
+++ src/store/rootReducer.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user/reducer";
 import galleries from "./galleries/reducer";
+import appState from "./appState/reducer";
 
 export default combineReducers({
+  appState,
   user,
   galleries,
 });
